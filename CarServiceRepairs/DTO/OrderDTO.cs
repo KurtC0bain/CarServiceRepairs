96342095a20c27f461574d221bd4cd638ece7581
KurtC0bain <jetpacker79@gmail.com>
2022-05-29 13:41:34 +0300

    Revert "Revert "add Order controller and CRUD operations""
    This reverts commit 3afd55bf2afbce332c307f43aedac7b37894fdb7.

---
+++ CarServiceRepairs/DTO/OrderDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace CarServiceRepairs.DTO
+{
+    public class OrderDTO
+    {
+        public string Breakage { get; set; }
+        public DateTime AdmissionDate { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime WarrantyEnd { get; set; }
+        public decimal Price { get; set; }
+        public int AutoId { get; set; }
+        public int WorkerId { get; set; }
+        public int DetailId { get; set; }
+        public int Amount { get; set; }
+    }
+}
